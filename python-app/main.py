0c6e568037a6ca0625b9bc10ca1ebf3acf335867
Viktoriya Nykytenko <kitakiv@gmail.com>
2025-07-30 13:58:54 +0300

    feat: add metrics to python

--- python-app/main.py
+++ python-app/main.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest
+from prometheus_client import start_http_server
 
 app = Flask(__name__)
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests')
 
 
 @app.route('/')
 def hello():
+    REQUEST_COUNT.inc()
     return 'Hello, World!'
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
